9f7ff1ac026e6812f90430d15b15f09933f0bb5a
Piet Ostendorp <79719028+Laubfrosch49@users.noreply.github.com>
2025-07-06 21:06:10 +0200

    Init

---
+++ src/main/java/dev/laubfrosch/bogensport/wedemarkteamopenapi/WedemarkTeamOpenApiApplication.java
@@ -0,0 +1,13 @@
+package dev.laubfrosch.bogensport.wedemarkteamopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedemarkTeamOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WedemarkTeamOpenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/laubfrosch/bogensport/wedemarkteamopenapi/WedemarkTeamOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.laubfrosch.bogensport.wedemarkteamopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedemarkTeamOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

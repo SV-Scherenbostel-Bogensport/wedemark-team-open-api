6687bba546754541c5fb7d82ba48b0277cd1a152
Piet Ostendorp <79719028+Laubfrosch49@users.noreply.github.com>
2025-07-21 22:59:42 +0200

    Fix response order

--- src/main/java/dev/laubfrosch/bogensport/wedemarkteamopenapi/api/repository/TeamRepository.java
+++ src/main/java/dev/laubfrosch/bogensport/wedemarkteamopenapi/api/repository/TeamRepository.java
@@ -12,12 +12,12 @@ import java.util.List;
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer> {
 
-    @Query("SELECT new dev.laubfrosch.bogensport.wedemarkteamopenapi.api.dto.AttendeesDto(t.teamId, t.name, t.playerCount, t.lookingForTeammates, t.hasPayed) FROM Team t")
+    @Query("SELECT new dev.laubfrosch.bogensport.wedemarkteamopenapi.api.dto.AttendeesDto(t.teamId, t.name, t.playerCount, t.lookingForTeammates, t.hasPayed) FROM Team t ORDER BY t.teamId")
     List<AttendeesDto> getAllTeams();
 
     @Query("SELECT count(t) FROM Team t")
     CountDto countTeams();
 
-    @Query("SELECT new dev.laubfrosch.bogensport.wedemarkteamopenapi.api.dto.GetTeamIdsDto(t.teamId, t.name) FROM Team t")
+    @Query("SELECT new dev.laubfrosch.bogensport.wedemarkteamopenapi.api.dto.GetTeamIdsDto(t.teamId, t.name) FROM Team t ORDER BY t.teamId")
     List<GetTeamIdsDto> getAllTeamIds();
 }
